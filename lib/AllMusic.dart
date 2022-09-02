961cfd41113be9df63b81ad2b94ccc26ca682fe2
ishangevariya <yamiishan1@gmail.com>
2022-09-02 09:36:52 +0530

    complete

--- lib/AllMusic.dart
+++ lib/AllMusic.dart
@@ -1,4 +1,6 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:songs/Model.dart';
 
 class AllMusic extends StatefulWidget {
   const AllMusic({Key? key}) : super(key: key);
@@ -8,10 +10,62 @@ class AllMusic extends StatefulWidget {
 }
 
 class _AllMusicState extends State<AllMusic> {
+
+  AudioPlayer player = AudioPlayer();
+
+  List<bool> statuslist = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    statuslist = List.filled(Model.SongList.length, false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: "All Music",),
+      appBar: AppBar(
+        title: Text("All Music"),
+      ),
+      body: ListView.builder(
+        itemCount: Model.SongList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+
+            trailing: statuslist[index] ?
+            IconButton(onPressed: () async {
+
+              setState(() {
+                statuslist = List.filled(Model.SongList.length, false);
+              });
+
+              print(statuslist);
+
+              await player.stop();
+
+            }, icon: Icon(Icons.pause))
+
+                :IconButton(onPressed: () async {
+              await player.stop();
+
+              setState(() {
+                statuslist = List.filled(Model.SongList.length, false);
+                statuslist[index] = true;
+              });
+
+              await player.play(DeviceFileSource(Model.SongList[index].data));
+
+              print(statuslist);
+
+
+            }, icon: Icon(Icons.play_arrow)),
+
+            title: Text("${Model.SongList[index].displayNameWOExt}"),
+            subtitle: Text("${Model.SongList[index].duration}"),
+          );
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:songs/Model.dart';
+
+import 'AllMusic.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -28,6 +31,11 @@ class _splashState extends State<splash> {
 
   loadSongs()
   async {
+
+    var status = await Permission.storage.status;
+    if (status.isDenied) {
+      await [Permission.storage].request();
+    }
 
    Model.SongList = await _audioQuery.querySongs();
 
